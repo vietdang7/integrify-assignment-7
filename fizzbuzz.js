8e7125d1e22b4b279c7cd25d8d38e474f889c4b1
Viet Dang Quoc <thedangs7@gmail.com>
2018-05-08 13:11:39 +0300

    new solution for fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,15 +1,24 @@
 var num;
-for (num = 1; num <= 100; num++){
-  if(num % 3 == 0 && num % 5 == 0) {
+for (num = 1; num <= 100; num++) {
+  if (num % 3 == 0 && num % 5 == 0) {
     console.log("Fizz Buzz");
-  }
-  else if (num % 3 == 0) {
+  } else if (num % 3 == 0) {
     console.log("Fizz");
-  }
-  else if (num % 5 == 0) {
+  } else if (num % 5 == 0) {
     console.log("Buzz");
-  }
-  else {
+  } else {
     console.log(num);
   }
+}
+
+/* Better way */
+for (let n = 1; n <= 100; n++) {
+  let output = "";
+  if (n % 3 === 0) {
+    output += "Fizz";
+  }
+  if (n % 5 === 0) {
+    output += "Buzz";
+  }
+  console.log(output || n);
 }

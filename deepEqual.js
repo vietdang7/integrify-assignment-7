78a71424593952d6c331161db874490ac6e189b7
Viet Dang Quoc <thedangs7@gmail.com>
2018-05-12 23:24:06 +0300

    finish deepEqual.js

--- deepEqual.js
+++ deepEqual.js
@@ -1,15 +1,24 @@
 var deepEqual = function(val1, val2) {
+  if (val1 === val2) {
+    return true;
+  }
   if (typeof val1 === typeof val2) {
     if (typeof val1 === "object") {
-      if (val1.value === val2.value) {
+      if (typeof val1 === null) {
         return true;
-      } else {
+      }
+      // learn this from classmate - Phuoc
+      val1Array = Object.keys(val1).sort();
+      val2Array = Object.keys(val2).sort();
+      if (val1Array.length !== val2Array.lenght) {
         return false;
       }
-    } else if (val1 === val2) {
+      for (let i = 0; i < val1Array.length; i++) {
+        if (val1Array[i] !== val2Array[i]) {
+          return false;
+        }
+      }
       return true;
-    } else {
-      return false;
     }
   }
 };
